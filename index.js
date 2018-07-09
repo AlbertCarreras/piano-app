4f1148b4baad9d532e54b14901945eb8a8652ea6
squamuglia <maxsmouha@gmail.com>
2018-07-09 15:54:51 -0400

    styled keys

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const ac = new AudioContext();
+const frequencyList = {"C": 32.70, "C#":34.65, "D":36.71, "Eb":38.89,	"E":41.20, "F":43.65, "F#":46.25,	"G":49.00, "G#":51.91,	"A":55.00,	"Bb":58.27,	"B":61.74}
+
 
 function playTone(note) {
+    let frequency = frequencyList[note]
+    let osc = ac.createOscillator();
 
-    const frequency = {
-      "0":
-        {"C": 16.35, "C#":17.32, "D":18.35, "Eb":19.45,	"E":20.60, "F":21.83, "F#":23.12,	"G":24.50, "G#":25.96,	"A":27.50,	"Bb":29.14,	"B":30.87},
-      "1":
-        {"C": 32.70, "C#":34.65, "D":36.71, "Eb":38.89,	"E":41.20, "F":43.65, "F#":46.25,	"G":49.00, "G#":51.91,	"A":55.00,	"Bb":58.27,	"B":61.74}
-      }
-
-    var osc = ac.createOscillator();
-
-    osc.type = 'sawtooth';
+    osc.type = 'square';
 
     osc.connect(ac.destination);
-
-    osc.frequency.value = note;
+    osc.frequency.value = frequency*2.5;
     osc.start();
     osc.stop(ac.currentTime + 0.5);
   }
 
   document.addEventListener('click',
   function(event) {
-      if (event.target.id === "start") {
+            if (event.target.className === "ivory") {
           let note = event.target.dataset.note
           playTone(note)
       }

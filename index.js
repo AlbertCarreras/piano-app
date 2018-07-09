660c7f2da6dd7d9d517c4792f63733a32ecf9ed1
squamuglia <maxsmouha@gmail.com>
2018-07-09 16:50:22 -0400

    vanilla js keyboard with keypresses

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const ac = new AudioContext();
-const frequencyList = {"C": 32.70, "C#":34.65, "D":36.71, "Eb":38.89,	"E":41.20, "F":43.65, "F#":46.25,	"G":49.00, "G#":51.91,	"A":55.00,	"Bb":58.27,	"B":61.74}
-const song1 = ["C", "D", "E", "F", "G", "A", "B", ""]
+const frequencyList = {"C": 32.70, "C#":34.65, "D":36.71, "Eb":38.89,	"E":41.20, "F":43.65, "F#":46.25,	"G":49.00, "G#":51.91,	"A":55.00,	"Bb":58.27,	"B":61.74} //frequency - key hash
+const keyValues = { "A":"C", "W":"C#", "S":"D", "E":"Eb", "D":"E", "F":"F" , "R":"F#", "G":"G" , "T":"G#", "H":"A" , "Y":"Bb", "J":"B" } //key - note hash for keypress
+
+const song1 = ["C", "D", "E", "F", "G", "A", "B", ""] //demo song
+
 
 function playTone(note, callback) {
     let frequency = frequencyList[note]
@@ -27,13 +30,18 @@ function playMelody(){
 	}
 }
 
-playMelody(song1) //plays song
 
+//plays note when clicking keyboard
 document.addEventListener('click',
   function(event) {
-            if (event.target.className === "ivory") {
-          let note = event.target.dataset.note
-          playTone(note)
-      }
-  }
+        if (event.target.className === "ivory") {
+            let note = event.target.dataset.note
+            playTone(note)
+        }
+    }
+)
+
+//plays note when pressing key
+document.addEventListener('keydown',
+(event) => {  playTone(keyValues[event.key.toUpperCase()])    }
 )

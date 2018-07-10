3537b6fa8a02bde20d546b9dcf42387441b1a86e
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-10 13:58:26 -0400

    buggy version

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const ac = new AudioContext();
 const frequencyList = {"C": 32.70, "C#":34.65, "D":36.71, "Eb":38.89,	"E":41.20, "F":43.65, "F#":46.25,	"G":49.00, "G#":51.91,	"A":55.00,	"Bb":58.27,	"B":61.74} //frequency - key hash
 const keyValues = { "A":"C", "W":"C#", "S":"D", "E":"Eb", "D":"E", "F":"F" , "R":"F#", "G":"G" , "T":"G#", "H":"A" , "Y":"Bb", "J":"B" } //key - note hash for keypress
 const noteObjects = {}
+const aBoolObjects = {}
 
 const song1 = ["C", "F#", "Eb", "F", "G", "A", "B"] //demo song
 let notes = []
@@ -21,30 +22,29 @@ function createNote(key) {
     osc.connect(ac.destination);
     osc.frequency.value = frequency*2.5;
     noteObjects[key] = osc;
+    aBoolObjects[key] = true;
 }
 
 //Plays note
-let aBool = true
-
 function playTone(note, callback) {
     let osc = noteObjects[note]
         document.getElementById(note).style="background: #fff7ae!important;" //highlights current note
         osc.start();
-        aBool = false
-        console.log('playtone ' + aBool)
+        aBoolObjects[note] = false;
+        console.log(aBoolObjects)
     if (callback) {stopTone(note, callback)}
 }
 
 function stopTone(note, callback) {
     let osc = noteObjects[note]
     osc.stop(ac.currentTime + 0.5);
-    aBool = true
-    console.log('stopTone '+ aBool)
+    aBoolObjects[note] = true;
+    console.log(aBoolObjects)
     createNote(note)
     if (callback) {osc.onended = function() {
         Array.from( document.getElementsByClassName('note')).forEach(element => element.style="")
         callback();
-         } 
+         }
     } else {
         Array.from( document.getElementsByClassName('note')).forEach(element => element.style="")
     }
@@ -53,9 +53,9 @@ function stopTone(note, callback) {
 //plays note when pressing key
 document.addEventListener('keydown',
     function (event) {
-        console.log('keydown ' + aBool)
-           if (aBool) {playTone(keyValues[event.key.toUpperCase()])}
-        } 
+        console.log('keydown ' + aBoolObjects[event.key.toUpperCase()])
+           if (aBoolObjects[event.key.toUpperCase()]) {playTone(keyValues[event.key.toUpperCase()])}
+        }
 )
 
 document.addEventListener('keyup',
@@ -87,4 +87,3 @@ document.addEventListener('click',
         }
     }
 )
-

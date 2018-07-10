1936dde54b2812ed9c648603235c9672ccebd1cb
squamuglia <maxsmouha@gmail.com>
2018-07-10 10:17:25 -0400

    trying to fix event listeners

--- index.js
+++ index.js
@@ -2,32 +2,37 @@ const ac = new AudioContext();
 const frequencyList = {"C": 32.70, "C#":34.65, "D":36.71, "Eb":38.89,	"E":41.20, "F":43.65, "F#":46.25,	"G":49.00, "G#":51.91,	"A":55.00,	"Bb":58.27,	"B":61.74} //frequency - key hash
 const keyValues = { "A":"C", "W":"C#", "S":"D", "E":"Eb", "D":"E", "F":"F" , "R":"F#", "G":"G" , "T":"G#", "H":"A" , "Y":"Bb", "J":"B" } //key - note hash for keypress
 
-const song1 = ["C", "D", "E", "F", "G", "A", "B", ""] //demo song
-
+const song1 = ["C#", "C#", "Eb", "F", "G", "A", "B", ""] //demo song
+let notes = []
 
 //Plays note
 function playTone(note, callback) {
     let frequency = frequencyList[note]
     let osc = ac.createOscillator();
 
-    osc.type = 'square'; //waveform for tone
+    osc.type = 'sawtooth'; //waveform for tone
 
     osc.connect(ac.destination);
     osc.frequency.value = frequency*2.5;
     osc.start();
-    osc.stop(ac.currentTime + 0.5);
-    osc.onended = callback;
+    document.addEventListener('keyup',
+        function() {osc.stop(ac.currentTime)}
+    )
+    if (callback) {osc.onended = callback}
 }
 
 //iterates over the array and plays the song
+function playSong(song) {
+    notes = [...song]
+    playMelody()
+}
 function playMelody(){
-	if (song1.length > 0){
-        var arr = Array.from( document.getElementsByClassName('ivory')).forEach(element => element.style="")
-
-        let note = song1.shift();
+	if (notes.length > 0){
+        Array.from( document.getElementsByClassName('note')).forEach(element => element.style="") //removes highlight on each pass
+        let note = notes.shift();
         console.log(note)
         playTone(note[0], playMelody);
-        document.getElementById(note).style="background: #fff7ae!important;"
+        document.getElementById(note).style="background: #fff7ae!important;" //highlights current note
 	}
 }
 
@@ -43,5 +48,8 @@ document.addEventListener('click',
 
 //plays note when pressing key
 document.addEventListener('keydown',
-(event) => {playTone(keyValues[event.key.toUpperCase()])}
+function (event) {
+    playTone(keyValues[event.key.toUpperCase()])
+    console.log(event)
+}
 )

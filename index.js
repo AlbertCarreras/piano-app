ecf4d785de26abfee32f1c00089512523ba72db4
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-11 09:28:43 -0400

    Added comments

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+//NEW AUDIOCONTEXT
 const ac = new AudioContext();
-const frequencyList = {"C": 32.70, "C#":34.65, "D":36.71, "Eb":38.89,	"E":41.20, "F":43.65, "F#":46.25,	"G":49.00, "G#":51.91,	"A":55.00,	"Bb":58.27,	"B":61.74} //frequency - key hash
+//CONSTANTS VARIABLES
+const frequencyList = {"C": 32.70, "C#":34.65, "D":36.71, "Eb":38.89,	"E":41.20, "F":43.65, "F#":46.25,	"G":49.00, "G#":51.91,	"A":55.00,	"Bb":58.27,	"B":61.74} // frequency - key hash
 const keyValues = { "A":"C", "W":"C#", "S":"D", "E":"Eb", "D":"E", "F":"F" , "R":"F#", "G":"G" , "T":"G#", "H":"A" , "Y":"Bb", "J":"B" } //key - note hash for keypress
 const noteObjects = {}
+//DYNAMIC VARIABLES
 const aBoolObjects = {}
 
 const song1 = ["C", "F#", "Eb", "F", "G", "A", "B"] //demo song
-let notes = []
+let notes = [] //temp storage variable for playing songs
 
-createNotes()
-
+//NOTES
+//builds noteObjects from frequencyList
 function createNotes() {
     Object.keys(frequencyList).forEach(
         key => createNote(key)
@@ -25,7 +28,10 @@ function createNote(key) {
     aBoolObjects[key] = true;
 }
 
-//Plays note
+createNotes()
+
+//PLAYING FUNCTIONALITY
+//Plays notes from playSong() and eventlisteners
 function playTone(note, callback) {
     let osc = noteObjects[note]
         document.getElementById(note).style="background: #fff7ae!important;" //highlights current note
@@ -50,20 +56,7 @@ function stopTone(note, callback) {
     }
 }
 
-//plays note when pressing key
-document.addEventListener('keydown',
-    function (event) {
-        console.log('keydown ' + aBoolObjects[keyValues[event.key.toUpperCase()]])
-           if (aBoolObjects[keyValues[event.key.toUpperCase()]]) {playTone(keyValues[event.key.toUpperCase()])}
-        }
-)
-
-document.addEventListener('keyup',
-   event => stopTone( keyValues[event.key.toUpperCase()] )
-
-)
-
-
+//REPLAY SONG FUNTIONALITY
 //iterates over the array and plays the song
 function playSong(song) {
     notes = [...song]
@@ -76,6 +69,20 @@ function playMelody(){
         playTone(note, playMelody);
 	}
 }
+
+//PLAY NOTES WITH EVENTLISTENERS
+//plays note when pressing key
+document.addEventListener('keydown',
+    function (event) {
+        console.log('keydown ' + aBoolObjects[keyValues[event.key.toUpperCase()]])
+           if (aBoolObjects[keyValues[event.key.toUpperCase()]]) {playTone(keyValues[event.key.toUpperCase()])}
+        }
+)
+
+document.addEventListener('keyup',
+   event => stopTone( keyValues[event.key.toUpperCase()] )
+
+)
 
 //plays note when clicking keyboard
 document.addEventListener('click',

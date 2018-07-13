ad44e019a2f2e600727f458e481c1b4be203ad93
squamuglia <maxsmouha@gmail.com>
2018-07-13 09:46:40 -0400

    cross-browser support

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //NEW AUDIOCONTEXT
-const ac = new AudioContext();
-
+const ac = new(window.AudioContext || 
+    window.webkitAudioContext || 
+    window.mozAudioContext || 
+    window.oAudioContext || 
+    window.msAudioContext);
+    
 //CONSTANTS VARIABLES
 const url = 'http://localhost:3000'
 const frequencyList = {"C": 32.70, "C#":34.65, "D":36.71, "Eb":38.89,	"E":41.20, "F":43.65, "F#":46.25,	"G":49.00, "G#":51.91,	"A":55.00,	"Bb":58.27,	"B":61.74} // frequency - key hash
